c48a7bb60c6516f97e825ffa17a1c34048c2ef11
Oleg Bakhirev <oleg.bakhirev@jetbrains.com>
2018-10-02 17:13:58 +0300

    Fix configuration state

--- src/app/app.js
+++ src/app/app.js
@@ -50,7 +50,8 @@ class Widget extends Component {
   initialize(dashboardApi) {
     dashboardApi.readConfig().then(config => {
       if (!config) {
-        return;
+        dashboardApi.enterConfigMode();
+        this.setState({isConfiguring: true});
       }
       this.setState(
         {isConfiguring: false,
@@ -70,9 +71,15 @@ class Widget extends Component {
   };
 
   cancelConfig = async () => {
-    this.setState({isConfiguring: false});
-    await this.props.dashboardApi.exitConfigMode();
-    this.initialize(this.props.dashboardApi);
+    const {dashboardApi} = this.props;
+    const config = await dashboardApi.readConfig();
+    if (!config) {
+      dashboardApi.removeWidget();
+    } else {
+      this.setState({isConfiguring: false});
+      await dashboardApi.exitConfigMode();
+      this.initialize(dashboardApi);
+    }
   };
 
   changeProjectId = e => this.setState({
